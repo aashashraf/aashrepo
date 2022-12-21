f1f9039d0bd59cc7c0a888ae19b2387dece8e4de
aashashraf <mohamedashrafali.j@gmail.com>
2022-12-21 03:18:10 +0000

    bbbb commit

--- LoanBookingSessionBean.java
+++ LoanBookingSessionBean.java
@@ -14,9 +14,6 @@ for(int i = 1; i <= size; i++)
 //outer loop      
 for(int j = 1; j <= size; j++)  
 {  
-//calculates and prints the values for pattern  
-System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");  
-}  
 System.out.println();  
 }  
 }  
